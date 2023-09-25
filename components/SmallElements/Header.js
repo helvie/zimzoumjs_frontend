0c7e9913f59fb6382b239723746c1895d283f544
Sylvie Helin <sylviepil1l1@gmail.com>
2023-09-25 11:04:04 +0200

    ajustements vercel

--- components/SmallElements/Header.js
+++ components/SmallElements/Header.js
@@ -1,15 +1,18 @@
 import stylesHeader from '../../styles/Header.module.css';
 
 import Burger from './Burger';
+import { useRouter } from 'next/router';
 
 ////////////////////////////////////////////////////////////////////////////////
 
 const Header = () => {
 
+  const router = useRouter();
+
   return (
     <div className={stylesHeader.headerContainer}>
       <div className={stylesHeader.headerImg}></div>
-      <div className={stylesHeader.headerLogo}></div>
+      <div style={{cursor:"pointer"}} onClick={()=>router.push('/')} className={stylesHeader.headerLogo}></div>
       <div className={stylesHeader.footerImg}></div>
       <Burger />
     </div>
